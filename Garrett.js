68916cd81f80e22f1f71d8f8dbc12eb4496adc76
garrettberthoumieux <garrett3231@gmail.com>
2017-07-11 13:29:22 -0400

    first commit

---
+++ Garrett.js
@@ -0,0 +1,2 @@
+console.log("Garrett is awesome!")
+node
---
+++ pokemonLineup.js
@@ -0,0 +1,7 @@
+var mypokemonlineup = ["Charizard", "Zekrom","Landorus","Mew", "Reshiram","Castform"]
+var firstoFight = mypokemonlineup[1];
+var secondtoFight = mypokemonlineup[3];
+mypokemonlineup.pop();
+mypokemonlineup.push("Kyerum");
+mypokemonlineup.push("Golurk");
+var rostersize = mypokemonlineup.length();
