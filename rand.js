58872927b97556c5a715e1124a21dad59e64f861
garrettberthoumieux <garrett3231@gmail.com>
2017-07-19 13:01:53 -0400

    battleship

---
+++ rand.js
@@ -0,0 +1,12 @@
+function rand(number){
+    var randomDecimalNumber = Math.random() * number;
+    console.log(randomDecimalNumber);
+
+    var wholeIntegerNumber = Math.round(randomDecimalNumber);
+
+
+    return wholeIntegerNumber;
+
+}
+
+console.log(rand(6));
